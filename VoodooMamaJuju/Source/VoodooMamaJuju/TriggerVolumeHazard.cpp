1dcddb7ea77f178123d25340513f53902c1e0249
Adam Poniatowski <adam_poniatowski@yahoo.com>
2016-04-26 19:26:22 -0500

    GAM-114: fixed bug with thought bubbles triggering player death and buff/debuff

--- VoodooMamaJuju/Source/VoodooMamaJuju/TriggerVolumeHazard.cpp
+++ VoodooMamaJuju/Source/VoodooMamaJuju/TriggerVolumeHazard.cpp
@@ -2,6 +2,22 @@
 
 #include "VoodooMamaJuju.h"
 #include "TriggerVolumeHazard.h"
+#include "Player/PlayerBase.h"
+
+
+void ATriggerVolumeHazard::OnBeginTriggerOverlap_Implementation(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if ((OtherActor != nullptr) && ((OtherActor->GetClass()->IsChildOf(APlayerBase::StaticClass()))))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString("Dead!"));
+
+		UGameplayStatics::OpenLevel(GetWorld(), "ContinueScreen");
+
+	}
+}
+
+
+
 
 
 
